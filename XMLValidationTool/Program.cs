f459633039112ad630fb8f958a5bddbb05e558bb
Niklas306 <71815719+Niklas306@users.noreply.github.com>
2024-02-23 17:50:10 +0100

    started witch XmlReader approach. Not working currently

--- XMLValidationTool/Program.cs
+++ XMLValidationTool/Program.cs
@@ -1,4 +1,7 @@
-﻿Console.WriteLine("Willkommen beim XML Validation Tool!\n\nGeben Sie den Pfad des XSD Schemas ein:");
+﻿using System.Xml;
+using System.Xml.Schema;
+
+Console.WriteLine("Willkommen beim XML Validation Tool!\n\nGeben Sie den Pfad des XSD Schemas ein:");
 
 string XSDschemaInput = Console.ReadLine();
 
@@ -6,6 +9,45 @@ Console.WriteLine("\nGeben Sie den Pfad der XML Datei zur Validierung ein:");
 
 string XMLfileInput = Console.ReadLine();
 
-Console.WriteLine("Starte Validierung...");
+if (!string.IsNullOrEmpty(XSDschemaInput) && !string.IsNullOrEmpty(XMLfileInput))
+{
+    Console.WriteLine("Starte Validierung...");
+    Validate(XSDschemaInput, XMLfileInput);
+}
 
 Console.ReadLine();
+
+
+void Validate(string xsdpath, string xmlpath)
+{
+    try
+    {
+        XmlReaderSettings settings = new XmlReaderSettings();
+        settings.ValidationType = ValidationType.Schema;
+        settings.ValidationFlags |= XmlSchemaValidationFlags.ProcessInlineSchema;
+        settings.ValidationFlags |= XmlSchemaValidationFlags.ProcessSchemaLocation;
+        settings.ValidationFlags |= XmlSchemaValidationFlags.ReportValidationWarnings;
+        settings.ValidationEventHandler += new ValidationEventHandler(ValidationCallback);
+
+        XmlReader reader = XmlReader.Create(xmlpath, settings);
+        XmlDocument document = new XmlDocument();
+        document.Load(reader);
+
+        document.Validate(ValidationCallback);
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine("Ausnahme: " + ex.Message + " ausgelöst!");
+    }
+}
+
+static void ValidationCallback(object sender, ValidationEventArgs args)
+{
+    if (args.Severity == XmlSeverityType.Warning)
+    {
+        Console.WriteLine("\tWarning: Matching schema not found. No validation occured." + args.Message);
+    } else
+    {
+        Console.WriteLine("\tValidation error: " + args.Message);
+    }
+}
